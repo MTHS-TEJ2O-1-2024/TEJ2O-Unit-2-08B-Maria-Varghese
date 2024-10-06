8b68c0117024ef5f5840cacbcb1c7a65e8aeabf6
Maria-Varghese <maria.varghese@mths.ca>
2024-10-06 09:55:46 -0700

    produces different colors in the RGB-led

--- main.ts
+++ main.ts
@@ -1,8 +1,60 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maria
+ * Created on: Sep 2024
+ * This program makes RGB LED scrolls through the colors it can procuce
 */
 
-basic.showString('Hello, World!')
+//setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//turn on the LED
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString('red')
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.showString('green')
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString('blue')
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.showString('yellow')
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString('cyan')
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString('purple')
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString('white')
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+})
+
